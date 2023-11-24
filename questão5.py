a19b8399e6df447258cf5401ae78b089a59d2746
Matheus Luiz Soares <matheus.luiz1@academico.ifrn.edu.br>
2023-11-24 20:23:45 -0300

    filtro

--- questão5.py
+++ questão5.py
@@ -2,7 +2,7 @@
 listalet = list()
 contador=0
 while contador<999:
-  n = int(input("Digite umalgum número aleatório inteiro e positivo até 999: "))   
+  n = int(input("Digite um número aleatório inteiro e positivo até 999: "))   
   listalet 
   contador+=1
   print(listalet)
