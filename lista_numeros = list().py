02b001fc983c66297c71bc28cc0b57e3da47d944
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2023-11-24 16:59:16 -0300

    testando

---
+++ lista_numeros = list().py
@@ -0,0 +1,9 @@
+lista_numeros = list()
+while True:
+
+  x = int(input("Digite algum valor positivo: "))
+  if x == 0:break
+  n = int(input("Digite algum valor inteiro: "))
+
+  lista_numeros.append((x, n))
+print(lista_numeros)
